18b30a39f1f9118100a7775f33c47cc7deb0ee21
TopScrech <sergei_saliukov@icloud.com>
2026-01-07 10:07:20 +0100

    Initial Commit

---
+++ RPC-Test/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RPC-Test
+//
+//  Created by Sergei Saliukov on 07.01.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RPC-Test/RPC_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  RPC_TestApp.swift
+//  RPC-Test
+//
+//  Created by Sergei Saliukov on 07.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct RPC_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
